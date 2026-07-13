b90ca1b0641ffd5195d8f4861b085ae7feebfdf2
Артем Гапеевцев <agapeevtsev@yandex.ru>
2026-07-13 17:01:39 +0800

    tariffs page

--- src/data/home-video-surveillance-tariffs.js
+++ src/data/home-video-surveillance-tariffs.js
@@ -3,24 +3,20 @@ export const homeVideoSurveillanceTariffs = [
     title: '1 камера',
     description: 'При тарифе от 100 Мбит/сек подключение бесплатное',
     buttonText: 'Подключить бесплатно*',
-    titleBig: true,
   },
   {
     title: '2 камеры',
     description: '200 ₽/мес. Архив записей 10 дней',
     buttonText: 'Подключить бесплатно*',
-    titleBig: true,
   },
   {
     title: 'От 3-x камер',
     description: 'Индивидуальный расчёт. Архив записей 10 дней',
     buttonText: 'Подключить бесплатно*',
-    titleBig: true,
   },
   {
     title: 'Монтаж вашей камеры',
     description: '2 000 ₽. Архив записей 10 дней',
     buttonText: 'Подключить бесплатно',
-    titleBig: true,
   },
 ];
