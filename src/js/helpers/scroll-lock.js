d06f4553d8e21434ba48ec7b47eacbf358d5fc79
Артем Гапеевцев <agapeevtsev@yandex.ru>
2026-07-29 18:27:04 +0800

    fix hover

--- src/js/helpers/scroll-lock.js
+++ src/js/helpers/scroll-lock.js
@@ -1,21 +1,31 @@
 const lockSources = new Set();
 const lockedClass = 'scroll-locked';
 
+let scrollY = 0;
+
 const getScrollbarWidth = () => window.innerWidth - document.documentElement.clientWidth;
 
 const applyScrollLock = () => {
   const shouldLock = lockSources.size > 0;
+  const isLocked = document.body.classList.contains(lockedClass);
 
-  if (shouldLock) {
+  if (shouldLock && !isLocked) {
+    scrollY = window.scrollY;
+
     const scrollbarWidth = getScrollbarWidth();
 
     document.documentElement.style.setProperty('--scrollbar-width', `${scrollbarWidth}px`);
+    document.body.style.top = `-${scrollY}px`;
     document.body.classList.add(lockedClass);
     return;
   }
 
-  document.body.classList.remove(lockedClass);
-  document.documentElement.style.removeProperty('--scrollbar-width');
+  if (!shouldLock && isLocked) {
+    document.body.classList.remove(lockedClass);
+    document.body.style.top = '';
+    document.documentElement.style.removeProperty('--scrollbar-width');
+    window.scrollTo(0, scrollY);
+  }
 };
 
 export function setScrollLocked(source, isLocked) {
