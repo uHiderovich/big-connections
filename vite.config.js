47634e7455c3bef2f84746f1c45f015e54d08e1a
Артем Гапеевцев <agapeevtsev@yandex.ru>
2026-09-24 11:53:20 +0800

    fix fonts, inline css

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,6 @@ import { dirname, extname, relative, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import nunjucks from 'nunjucks';
-import * as sass from 'sass';
 import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
@@ -32,12 +31,6 @@ const pagesDir = resolve(projectRoot, 'src/pages');
 const templatesDir = resolve(projectRoot, 'src/templates');
 const imagesDir = resolve(projectRoot, 'src/assets/images');
 const publicDir = resolve(projectRoot, 'public');
-const stylesDir = resolve(projectRoot, 'src/assets/scss');
-const baseDir = resolve(stylesDir, 'base');
-const criticalStylesFile = resolve(stylesDir, 'main.scss');
-const criticalScssFiles = new Set([
-  criticalStylesFile,
-]);
 
 const IMAGE_EXTENSIONS = new Set([
   '.avif',
@@ -76,13 +69,6 @@ function getHtmlInputs() {
       [name]: filePath,
     };
   }, {});
-}
-
-function getCriticalCss() {
-  return sass.compile(criticalStylesFile, {
-    loadPaths: [baseDir],
-    style: 'compressed',
-  }).css;
 }
 
 function relocateDistDirectory(fromDir, toDir) {
@@ -135,10 +121,6 @@ function shouldFullReload(file) {
     return true;
   }
 
-  if (extension === '.scss' && criticalScssFiles.has(normalized)) {
-    return true;
-  }
-
   if (normalized.startsWith(imagesDir) && IMAGE_EXTENSIONS.has(extension)) {
     return true;
   }
@@ -154,7 +136,7 @@ function nunjucksHmr() {
   return {
     name: 'nunjucks-hmr',
     configureServer(server) {
-      const watchDirs = [pagesDir, templatesDir, stylesDir, baseDir, imagesDir, publicDir].filter((dir) =>
+      const watchDirs = [pagesDir, templatesDir, imagesDir, publicDir].filter((dir) =>
         existsSync(dir),
       );
 
@@ -250,7 +232,6 @@ function nunjucksHtml() {
           : '';
 
         return env.renderString(html, {
-          criticalCss: getCriticalCss(),
           isDev: Boolean(context.server),
           pagePath,
         });
